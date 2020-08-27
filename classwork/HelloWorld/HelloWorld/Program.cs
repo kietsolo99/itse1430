b20a3a5ff9ff6476f6bc141c6684b767eb97f7ec
Kiet Vo <kiet.vo054@my.tccd.edu>
2020-08-26 21:35:34 -0500

    playing around with C#

--- classwork/HelloWorld/HelloWorld/Program.cs
+++ classwork/HelloWorld/HelloWorld/Program.cs
@@ -7,20 +7,46 @@ using System;
 
 namespace HelloWorld
 {
+    //pascal casing - Capitalize on word boundaries including first word
+    //Camel casing - Capitalize on word boudaries except first (e.g firstName, payRate)
     class Program
     {
-        static void Main(string[] args)
+        //Function declaration - decleration to complier that a function exists with a given signature
+        //Funtion signature - funtion name and the parameter types (somtimes it will include return type)
+        //Funtion definition - declaration + implementation
+        static void Main(string[] args) // 1 parameter
         {
             // Comments go above the block of code
-            Console.WriteLine("Hello World!");
 
-            int hours = 40;
+            //Display given string to output
+            //Arguments - data passed to funtion
+            Console.WriteLine("Hello World!"); // same as printf , cout
 
-            double payRate = 12.80;
+            //Variable declaration;
+            // Type id;
+            //int hours;
 
-            double totalPay = hours * payRate;
-            //Another block
-            //NO {Console.WriteLine();}
+            //hours = 10; // Assignment operator: id = E
+
+            //Identifier Rules:
+            //1. Unique within scope
+            //2. Must start with underscore or letter
+            //3. Consists of letters, digits and underscores
+            //eg.
+            //firstName, first_Name, first_name, pay1rate, NOT 1payRate
+            //Always Camel case local variables
+            //Preferred: T id = E
+            int hours = 10;
+
+            //int pay = 0;
+
+            //pay = hours * 9;
+            int totalPay = hours * 9;
+
+            //Function overloading - multiple function with  same name but different parameter
+            // atof, atoi
+            Console.WriteLine(totalPay);
+
         }
     }
 }
